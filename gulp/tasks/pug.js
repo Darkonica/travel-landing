59fa2dc2673dbb8f3b79d938aa7d87b1be32cbec
Vladimir Novichikhin <darkonica@gmail.com>
2019-01-10 03:42:36 +0300

    gulp fixes + base layout

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,10 +1,16 @@
 module.exports = function() {
-    $.gulp.task("pug", function() {
-        return $.gulp.src(["./src/views/**/index.pug", "!./src/views/blocks/*.pug"])
-            .pipe($.pug({pretty: true}))
-            .pipe($.replace("../dest/", "../"))
-            .pipe($.gulp.dest("./dest/"))
-            .pipe($.debug({"title": "html"}))
-            .on("end", $.browsersync.reload);
-    });
-};
+  $.gulp.task('pug', function() {
+    return $.gulp
+      .src([
+        './src/views/**/index.pug',
+        '!./src/views/**/layout.pug',
+        '!./src/views/blocks/*.pug',
+        '!./src/views/includes/*.pug',
+      ])
+      .pipe($.pug({ pretty: true }))
+      .pipe($.replace('../dest/', '../'))
+      .pipe($.gulp.dest('./dest/'))
+      .pipe($.debug({ title: 'html' }))
+      .on('end', $.browsersync.reload)
+  })
+}
